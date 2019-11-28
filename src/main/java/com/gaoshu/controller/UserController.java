3d324c49b44a77da6172bab4da554104082552e6
younfuckinghoo <howyong0419@hotmail.com>
2019-11-28 11:01:58 +0800

    UserController添加url映射以及创建对象的注解

--- src/main/java/com/gaoshu/controller/UserController.java
+++ src/main/java/com/gaoshu/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.gaoshu.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  *
  */
+@RequestMapping("user")
+@Controller
 public class UserController {
+
+
+
 }
