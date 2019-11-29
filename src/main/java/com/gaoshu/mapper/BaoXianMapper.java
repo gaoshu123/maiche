1c5adb0c84b3ecbc8a6a99dd6a2fa47b4d849df2
gaoshu123 <896312840@qq.com>
2019-11-29 11:46:15 +0800

    增加保险订单实体类

---
+++ src/main/java/com/gaoshu/mapper/BaoXianMapper.java
@@ -0,0 +1,15 @@
+package com.gaoshu.mapper;
+
+import com.gaoshu.pojo.BaoXianDingDan;
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Administrator on 2019/11/29.
+ */
+@Mapper
+@Repository public interface BaoXianMapper extends tk.mybatis.mapper.common.Mapper<BaoXianDingDan> {
+
+
+}
